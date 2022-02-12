14356f8c2bb635097587e86ff38079dbb687b37d
Michael Paukov <michael.paukov@gmail.com>
2022-02-12 20:50:14 +0100

    start work

---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,23 @@
+import s from './Searchbar.module.css';
+
+const Searchbar = () => {
+  return (
+    <header className={s.Searchbar}>
+      <form className={s.SearchForm}>
+        <button type="submit" className={s.SearchFormButton}>
+          <span className={s.SearchFormButtonLabel}>Search</span>
+        </button>
+
+        <input
+          className={s.SearchFormInput}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
+
+export default Searchbar;
