68b5fe00874bfec2723064c4f9bc4736a19ca418
Michael Paukov <michael.paukov@gmail.com>
2022-02-17 17:30:21 +0100

    add btn loadmore

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,4 +1,5 @@
 import { ServiceAPI } from 'components/API';
+import { Button } from 'components/Button';
 import { Component } from 'react';
 import { Watch } from 'react-loader-spinner';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
@@ -14,25 +15,38 @@ class ImageGallery extends Component {
   };
 
   componentDidUpdate(prevProps, prevState) {
-    if (
-      this.props.query !== prevProps.query ||
-      this.state.page !== prevState.page
-    ) {
+    if (this.props.query !== prevProps.query) {
+      this.setState({ status: 'pending', response: [], page: 1 });
+      this.getPicture();
+    }
+    if (this.state.page !== prevState.page) {
       this.setState({ status: 'pending' });
-      ServiceAPI(this.props.query, this.page)
-        .then(this.dataProcessing)
-        .catch(error => this.setState({ error, status: 'rejected' }));
+      this.getPicture();
     }
   }
+
+  getPicture = () => {
+    ServiceAPI(this.props.query, this.state.page)
+      .then(this.dataProcessing)
+      .catch(error => this.setState({ error, status: 'rejected' }));
+  };
 
   dataProcessing = response => {
     const data = response.data.hits.map(data => {
       const { id, largeImageURL: imageURL, webformatURL: src, tag: alt } = data;
       return { id, imageURL, src, alt };
     });
-    return this.setState({
-      response: data,
-      status: 'resolved',
+    return this.setState(prevState => {
+      return {
+        response: [...prevState.response, ...data],
+        status: 'resolved',
+      };
+    });
+  };
+
+  handleLoadMore = () => {
+    this.setState(prevState => {
+      return { page: prevState.page + 1 };
     });
   };
 
@@ -49,9 +63,12 @@ class ImageGallery extends Component {
 
     if (status === 'resolved') {
       return (
-        <ul className={s.ImageGallery}>
-          <ImageGalleryItem images={this.state.response} />
-        </ul>
+        <>
+          <ul className={s.ImageGallery}>
+            <ImageGalleryItem images={this.state.response} />
+          </ul>
+          <Button onClick={this.handleLoadMore} />
+        </>
       );
     }
     if (status === 'pending') {
